84759e045050115bf3733ebd9eef59e8071e7a36
bellabeen <rootsquid@gmail.com>
2020-03-03 12:57:05 +0700

    FIX GRAFIK

--- tanah/stats.php
+++ tanah/stats.php
@@ -55,7 +55,7 @@ $resultAll= isset($getAll['data']) ? $getAll['data'] : [];
 		foreach($resultAll as $result){
 			// echo($result['suhu']);'<br>';
 			// $data_suhu[] = array(strtotime($result['waktu']),$result['suhu']);
-			array_push($data_suhu,array(strtotime($result['waktu']),$result['suhu']));
+			array_push($data_suhu,array(strtotime($result['waktu']),(float) $result['suhu']));
 
 		}
 		// die (json_encode($data_suhu));
@@ -85,37 +85,12 @@ $resultAll= isset($getAll['data']) ? $getAll['data'] : [];
 							   }
 					},
 					  series: [{	
-						data: [
-							// [<?php echo json_encode($data_suhu);?>],
-								[Date.UTC(1970, 10, 25), 0],
-								[Date.UTC(1970, 11,  6), 0.25],
-								[Date.UTC(1970, 11, 20), 1.41],
-								[Date.UTC(1970, 11, 25), 1.64],
-								[Date.UTC(1971, 0,  4), 1.6],
-								[Date.UTC(1971, 0, 17), 2.55],
-								[Date.UTC(1971, 0, 24), 2.62],
-								[Date.UTC(1971, 1,  4), 2.5],
-								[Date.UTC(1971, 1, 14), 2.42],
-								[Date.UTC(1971, 2,  6), 2.74],
-								[Date.UTC(1971, 2, 14), 2.62],
-								[Date.UTC(1971, 2, 24), 2.6],
-								[Date.UTC(1971, 3,  1), 2.81],
-								[Date.UTC(1971, 3, 11), 2.63],
-								[Date.UTC(1971, 3, 27), 2.77],
-								[Date.UTC(1971, 4,  4), 2.68],
-								[Date.UTC(1971, 4,  9), 2.56],
-								[Date.UTC(1971, 4, 14), 2.39],
-								[Date.UTC(1971, 4, 19), 2.3],
-								[Date.UTC(1971, 5,  4), 2],
-								[Date.UTC(1971, 5,  9), 1.85],
-								[Date.UTC(1971, 5, 14), 1.49],
-								[Date.UTC(1971, 5, 19), 1.27],
-								[Date.UTC(1971, 5, 24), 0.99],
-								[Date.UTC(1971, 5, 29), 0.67],
-								[Date.UTC(1971, 6,  3), 0.18],
-								[Date.UTC(1971, 6,  4), 0]
-
-						]
+						data: 
+							<?php echo json_encode($data_suhu);?>
+								// [Date.UTC(1970, 10, 25), 0],
+								// [Date.UTC(1970, 11,  6), 0.25],
+								// [Date.UTC(1970, 11, 20), 1.41],
+								// [Date.UTC(1970, 11, 25), 1.64]
 					  }]
 			   });
 			 </script>
