69530fdf8e3f6c618b0f6b67d279fd392c271702
bellabeen <rootsquid@gmail.com>
2020-05-14 14:44:44 +0700

    Update db TA

--- conf/config.php
+++ conf/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST","localhost");
-define("DB_USER","bellabeen");
-define("DB_PASSWORD","kepoajalu_1851");
+define("DB_USER","root");
+define("DB_PASSWORD","");
 define("DB_NAME","3074218_latihanarjun");
 ?>
 
